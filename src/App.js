97aa24bff8f1bd44fe5e05189d33344ac931677e
PopovDS617 <popovds.bm@gmail.com>
2022-08-01 20:00:14 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+function App() {
+  const [loadedMovies, setLoadedMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = useCallback(async () => {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        director: movieData.director,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    setLoadedMovies(transformedMovies);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading . . .</p>;
+  }
+
+  // if (!isLoading && loadedMovies.length > 0) {
+  //   content = loadedMovies.map((movie) => {
+  //     return (
+  //       <tr>
+  //         <th> {movie.title}</th>
+  //         <th> {movie.director}</th>
+  //       </tr>
+  //     );
+  //   });
+  // }
+
+  if (!isLoading && loadedMovies.length < 1) {
+    content = <p>found no movies</p>;
+  }
+
+  return (
+    <div>
+      StarWars API
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Realease date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!isLoading &&
+            loadedMovies.length > 0 &&
+            loadedMovies.map((movie) => {
+              return (
+                <tr>
+                  <th> {movie.title}</th>
+                  <th> {movie.director}</th>
+                  <th> {movie.releaseDate}</th>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
